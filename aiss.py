e23098e4bf2c53edef320da586b54b03f9682f19
You Xuan <youxuan2010@gmail.com>
2024-03-16 17:51:39 +0800

    first commit

---
+++ aiss.py
@@ -0,0 +1,221 @@
+from openai import OpenAI
+import openai
+import os
+from PIL import Image
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import cv2
+import time
+import datetime
+
+
+client = OpenAI(base_url='http://localhost:1234/v1', api_key='not-needed')
+
+def mistral7b(user_input):
+    try:
+        streamed_completion = client.chat.completions.create(model="local-model", 
+                                                            messages=[{"role": "system", "content": "You expert at writing security logs. Write the log using the given description."}, 
+                                                                      {"role": "user", "content": user_input}],
+                                                            stream=True)  
+        full_response = ""
+        line_buffer = ""
+
+        for chunk in streamed_completion:
+            delta_content = chunk.choices[0].delta.content
+            if delta_content is not None:
+                line_buffer += delta_content
+
+                if '\n' in line_buffer:
+                    lines = line_buffer.split('\n')
+                    for line in lines[:-1]:
+                        full_response += line + '\n'
+                    line_buffer = lines[-1]
+
+        if line_buffer:
+            # # Add timestamp and date to the remaining buffer
+            # timestamp = datetime.datetime.now().strftime("%H:%M:%S")
+            # date = datetime.datetime.now().strftime("%Y-%m-%d")
+            # line_with_timestamp = f"[{date} {timestamp}] {line_buffer}"
+            # print(line_with_timestamp)  # Print line with timestamp for debugging
+            full_response += line_buffer
+
+        return full_response, True
+    
+    except openai.APIConnectionError as e:
+        print("Error connecting to OpenAI API:", e , "Please check LM Studio is running and accessible at the specified URL")
+        return str(e), False
+	
+def process_image(images_dir):
+    # Find all images in the directory
+    images = [os.path.join(images_dir, f) for f in os.listdir(images_dir) if f.endswith('.jpg') or f.endswith('.png')]
+
+    if not images:
+        print('No images found in the directory')
+        return None, None
+    
+    # Sort the images by modification time
+    latest_images = max(images, key=os.path.getmtime)
+
+    for image in images:
+        if image != latest_images:
+            os.remove(image)
+            print(f"Removed {image}")
+
+    # Process the latest image
+    model_id = "vikhyatk/moondream2"
+    revision = "2024-03-06"
+    model = AutoModelForCausalLM.from_pretrained(model_id, trust_remote_code=True, revision=revision)
+    tokenizer = AutoTokenizer.from_pretrained(model_id, revision=revision)
+
+    image = Image.open(latest_images)
+    enc_image = model.encode_image(image)
+
+    return model, enc_image, tokenizer
+
+def start_video_capture(stream_url, output_dir, capture_duration=5):
+    """
+    Captures video from the stream for the given duration and saves it as .mp4 using H.264 codec.
+    Returns the path to the captured video.
+
+    :param stream_url: URL of the video stream
+    :param output_dir: Directory to save the captured video
+    :param capture_duration: Duration of the video to capture
+    """
+    ensure_dir_exists(output_dir)
+
+    cap = cv2.VideoCapture(stream_url)
+
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1270)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+
+    if not cap.isOpened():
+        print("Error: Could not open video stream")
+        return None
+
+    # Get actual frame size
+    frame_width = int(cap.get(3))
+    frame_height = int(cap.get(4))    
+
+    # Define the codec and create VideoWriter object
+    # Attempt to use H.264 codec ('X264' or 'avc1') for better compression
+    # Note: Might need to change the codec based on the system
+    fourcc = cv2.VideoWriter_fourcc(*'X264')
+
+    output_path = os.path.join(output_dir, f'captured_video_{datetime.datetime.now().strftime("%Y%m%d_%H%M%S")}.mp4')
+    out = cv2.VideoWriter(output_path, fourcc, 20.0, (frame_width, frame_height))
+
+    start_time = time.time()
+    print("Started capturing video...")
+
+    while int(time.time() - start_time) < capture_duration:
+        ret, frame = cap.read()
+        if not ret:
+            print("Failed to capture frame")
+            break
+        out.write(frame)
+        # cv2.imshow('Capturing Video', frame)
+        # if cv2.waitKey(1) & 0xFF == ord('q'):
+        #     break
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
+    print(f'Video captured and saved at {output_path}')
+
+    return output_path
+
+def ensure_dir_exists(dir_path):
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+
+def capture_frame(stream_url, images_dir='images'):
+    # Capture a single frame from the video stream and save it into a directory
+    cap = cv2.VideoCapture(stream_url)
+
+    if not cap.isOpened():
+        print("Error: Could not open video stream")
+        return 0
+    
+    ensure_dir_exists(images_dir)
+    ret, frame = cap.read()
+    if not ret:
+        print("Failed to capture frame")
+    else:
+        filename = os.path.join(images_dir, f'frame_{int(time.time())}.jpg')
+        cv2.imwrite(filename, frame)
+        print(f'Frame captured and saved at {filename}')
+    
+    cap.release()
+    return 1
+
+def save_file(file_path, text):
+    try:
+        with open(file_path, 'a') as file:
+            file.write(text + '\n')
+    except Exception as e:
+        print(f"Error occurred while saving file: {e}")
+    
+def main():
+
+    stream_url = 'http://192.168.0.103:8080/video'
+    images_dir = "C:/Users/youxu/Desktop/AI Surveillance System/images"
+    video_dir = "C:/Users/youxu/Desktop/AI Surveillance System/video"
+
+    person_detected = False
+
+    while True:
+        if capture_frame(stream_url, images_dir=images_dir):
+            print("Frame captured. Processing the image...")
+            text_model, image_embeds, tokenizer = process_image(images_dir)
+            prompt = "Is there a PERSON in the image? (ONLY ANSWER WITH YES OR NO)"
+            # prompt = "Is there a zombie in the image? (ONLY ANSWER WITH YES OR NO)"
+            print(">", prompt)
+
+            # Ensure text_model and image_embeds are not valid
+            if text_model is not None and image_embeds is not None:
+                answer = text_model.answer_question(image_embeds, prompt, tokenizer).strip().upper()
+            else:
+                print("Could not process the image")
+                continue
+                
+            print("Response:", answer)
+
+            if answer == "YES":
+                person_detected = True
+                print("Person detected! Starting video capture...")
+                video_path = start_video_capture(stream_url, output_dir=video_dir, capture_duration=5)
+
+                # text_model, image_embeds, tokenizer2 = process_image(images_dir)
+                prompt2 = "Describe the image in details, try to identify Gender, Objects, Clothing color, Shoes, Environment, Weather etc:"
+                # prompt2 = "Describe the gameplay image in details, try to identify what is happening in the scene"
+                answer2 = text_model.answer_question(image_embeds, prompt2, tokenizer)
+                print("Image Description:", answer2)
+                timestamp = datetime.datetime.now().strftime("%H:%M:%S")
+                date = datetime.datetime.now().strftime("%Y-%m-%d")
+                log = f"[{date} {timestamp}] Image Description: {answer2}"
+                # log = "Image Description: " + answer2
+                log2, success = mistral7b(log)
+                if success:
+                    print(log2)
+                    save_file('Security_Logs.txt', log2)
+                else:
+                    print(log2)
+                    return False
+
+                print("Notifying user via Telegram...")
+                return True
+
+            elif answer == "NO":
+                person_detected = False
+            else:
+                print("Invalid response. Please answer with YES or NO")
+                continue
+
+            print("Person detected:", person_detected)
+        else:
+            print("Failed to capture frame. Please check the video stream.")
+            break
+    return False
+            
+
+# Run the main coroutine
+if __name__ == "__main__":
+    main()
---
+++ telebot.py
@@ -0,0 +1,215 @@
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters
+from dotenv import load_dotenv
+from datetime import datetime
+from telegram import InlineKeyboardMarkup, InlineKeyboardButton
+from telegram.ext import CallbackQueryHandler
+import aiss
+import os
+import glob
+import cv2
+import asyncio
+
+# Load environment variables from .env file
+load_dotenv()
+token = os.getenv('TELEGRAM_BOT_TOKEN')
+
+message_text = ""
+video_path = ""
+
+def print_message(update: Update, action: str):
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    first_name = update.message.from_user.first_name
+    last_name = update.message.from_user.last_name
+
+    message = f"[{timestamp}] Action '{action}' triggered in chat ID {update.effective_chat.id}"
+
+    if first_name and last_name:
+        message += f" by {first_name} {last_name}"
+    elif first_name:
+        message += f" by {first_name}"
+    elif last_name:
+        message += f" by {last_name}"
+
+    print(message)
+
+def get_latest_video_file(directory):
+    video_files = glob.glob(os.path.join(directory, '*.mp4'))
+    if not video_files:
+        return None
+    latest_video_file = max(video_files, key=os.path.getmtime)
+    return latest_video_file
+
+def check_camera_status(stream_url):
+    cap = cv2.VideoCapture(stream_url)
+
+    if not cap.isOpened():
+        return False
+    else:
+        cap.release()
+        return True
+
+# Define a callback function to handle inline keyboard button presses
+async def button_callback(update, context):
+    query = update.callback_query
+    user_response = query.data
+
+    # Check the callback data to determine the user's choice
+    if user_response == 'yes':
+        # Read the security log from the file
+        with open('Security_Logs.txt', 'r') as file:
+            security_log = file.read()
+
+        # Send the security log to the user
+        await query.answer()
+        await query.message.reply_text(text=security_log)
+    elif user_response == 'no':
+        await query.answer()
+        await query.message.reply_text(text="Okay, no problem!")
+
+    # Edit the message to remove the inline keyboard
+    await query.message.edit_reply_markup(reply_markup=None)
+
+async def send_message(update, context, timeout=15):
+    print_message(update, 'sending_message_to_user')
+    
+    # Create Bot instance
+    bot = context.bot
+
+    # Specify the directory to search for video files
+    video_directory = "C:/Users/youxu/Desktop/AI Surveillance System/video"
+
+    # Get the latest video file path
+    video_file_path = get_latest_video_file(video_directory)
+
+    if video_file_path is None:
+        print("No video files found in the directory")
+        return
+    
+    # Send message
+    response = await bot.send_message(chat_id=update.effective_chat.id, text="Person Detected in Frame! Sending Video...")
+
+    try:
+        # Attempt to send the video within the specified timeout
+        # Send video
+        with open(video_file_path, 'rb') as video_file:
+            await asyncio.wait_for(bot.send_video(chat_id=update.effective_chat.id, video=video_file, width= 1270, height=720), timeout=timeout)
+        
+        # Define the inline keyboard markup for asking the user
+        keyboard = [
+            [InlineKeyboardButton("Yes", callback_data='yes'), InlineKeyboardButton("No", callback_data='no')]
+        ]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+
+        # Ask the user if they want to view the current security log
+        await bot.send_message(chat_id=update.effective_chat.id, 
+                               text="Do you want to view the current security log?", 
+                               reply_markup=reply_markup)
+
+    except asyncio.TimeoutError:
+        # Handle timeout error
+        print("Sending video timed out")
+    
+async def start_server(update: Update, context):
+    global message_text, video_path
+
+    print_message(update, 'start_server')
+    
+    message = 'Starting the AI security server...'
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+    
+    # Stsrting the AI security server
+    if aiss.main():
+        await send_message(update, context)
+    else:
+        message = 'Failed to start the AI security server, please check the camera and LM Studio server status.'
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+
+        
+async def start(update: Update, context):
+    print_message(update, 'start')
+    response = await context.bot.send_message(
+        chat_id=update.effective_chat.id, 
+        text='Hello and welcome to your personal AI Surveillance System! Use /help to see the list of supported commands.'
+    )
+
+async def help(update: Update, context):
+    print_message(update, 'help')
+    response = await context.bot.send_message(
+        chat_id=update.effective_chat.id, 
+        text="""
+        The BuiltIn Telegram bot supports the following commands:
+- /help: List of supported commands (you are here)
+- /first_name: Reports the user's first name
+- /last_name: Reports the user's last name
+- /start_server: Start the AI security server
+- /check_job_status: Checks your job status
+- /live: View the live camera feed
+        """
+    )
+
+async def first_name(update: Update, context):
+    print_message(update, 'first_name')
+    response = await context.bot.send_message(
+        chat_id=update.effective_chat.id, 
+        text=f'Your first name is {update.message.from_user.first_name}'
+    )
+    
+async def last_name(update: Update, context):
+    print_message(update, 'last_name')
+    response = await context.bot.send_message(
+        chat_id=update.effective_chat.id, 
+        text=f'Your last name is {update.message.from_user.last_name}'
+    )
+    
+async def check_job_status(update: Update, context):
+    print_message(update, 'check_job_status')
+    message = "You are jobless. Try harder NOOB!"
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+    
+    # Send sticker image
+    photo_path = "Asset/prank.png"
+    with open(photo_path, 'rb') as photo:
+        await context.bot.send_photo(chat_id=update.effective_chat.id, photo=photo)
+
+async def live(update, context):
+    print_message(update, 'live')
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Connecting to the live camera feed...")
+
+    live_stream_url = "http://192.168.0.103:8080/video"
+
+    camera_status = check_camera_status(live_stream_url)
+    if camera_status:
+        message = "Camera is live. Here is the live feed:\n" + live_stream_url
+    else:
+        message = "Error: Camera is not working. Please check the camera connection."
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+
+
+if __name__ == '__main__':
+    application = Application.builder().token(token).build()
+
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+
+    help_handler = CommandHandler('help', help)
+    application.add_handler(help_handler)
+
+    first_name_handler = CommandHandler('first_name', first_name)
+    application.add_handler(first_name_handler)
+
+    last_name_handler = CommandHandler('last_name', last_name)
+    application.add_handler(last_name_handler)
+
+    start_server_handler = CommandHandler('start_server', start_server)
+    application.add_handler(start_server_handler)
+
+    check_job_status_handler = CommandHandler('check_job_status', check_job_status)
+    application.add_handler(check_job_status_handler)
+
+    live_handler = CommandHandler('live', live)
+    application.add_handler(live_handler)
+
+    application.add_handler(CallbackQueryHandler(button_callback))
+
+    application.run_polling()
